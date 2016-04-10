24edcac2d3cabdfcf1413ee84f290cc48c41986a
Vignesh V <tovigu@gmail.com>
2016-04-10 23:21:28 +0530

    Create Mul.java

---
+++ Mul.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Mul {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int j,n;
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter the integer > ");
+		n = in.nextInt();
+		for(j=2;j<n;j++){
+			if(n%j==0){
+				System.out.println("Not a prime");
+				break;
+			}
+		}
+		if(j==n)
+			System.out.println("A prime!");
+	}
+
+}
